b1de2c7b96fa2ea093d2cb547ddc95511272af1e
Vũ Văn Đạt <58060750+Okami00@users.noreply.github.com>
2021-05-30 17:13:06 +0700

    Second Commit

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 // Object question
 var quiz = [
-    { question: "2 x 3 = ?", choices: [1, 2, 6, 4], correctAnswer: 6 },
-    { question: "3 x 4 = ?", choices: [12, 2, 3, 4], correctAnswer: 12 },
-    { question: "4 x 5 = ?", choices: [1, 20, 3, 4], correctAnswer: 20 },
-    { question: "5 x 6 = ?", choices: [1, 2, 30, 4], correctAnswer: 30 }
+    { question: "2 x 1 = ?", choices: [1, 2, 3, 4], correctAnswer: 2 },
+    { question: "2 x 2 = ?", choices: [3, 4, 5, 6], correctAnswer: 4 },
+    { question: "2 x 3 = ?", choices: [6, 7, 8, 9], correctAnswer: 6 },
+    { question: "2 x 4 = ?", choices: [7, 8, 9, 10], correctAnswer: 8 },
+    { question: "2 x 5 = ?", choices: [8, 10, 9, 11], correctAnswer: 10 },
+    { question: "2 x 6 = ?", choices: [9, 10, 6, 12], correctAnswer: 12 },
+    { question: "2 x 7 = ?", choices: [14, 12, 18, 20], correctAnswer: 14 }
 ];
 // Variable
 var onStart = document.querySelector('.start__btn');
 var notification = document.querySelector(".notification");
 var quesDetail = document.querySelector('.question__detail');
+var time = document.getElementById("time");
 var countdown;
 // When click button start => time countdown
 function startClick() {
     onStart.disabled = true;
     disButton(false);
     countdown = setInterval(function () {
-        var seconds = document.getElementById("time").textContent;
+        var seconds = time.textContent;
         seconds--;
-        document.getElementById("time").innerHTML = seconds;
+        time.innerHTML = seconds;
         if (seconds <= 0) {
             clearInterval(countdown); // ngắt lặp lại
             notification.style.display = "inline";
-            document.getElementById("time").innerHTML = "10";
             disButton(true);
         }
     }, 1000);
@@ -29,23 +32,17 @@ function startClick() {
 // Onclick Ok Notification
 function timeUp() {
     notification.style.display = "none";
-    onStart.disabled = false;
-    disButton(true);
-    count = 0;
-    score = 0;
-    document.querySelector('.score span').innerHTML = "0";
-    createQuestionElement(count);
+    reset()
 }
 // Restart
-function restart() {
+function reset() {
     score = 0;
-    document.querySelector('.score span').innerHTML = score;
-    document.getElementById("time").innerHTML = "10";
-    onStart.disabled = false;
-    disButton(true);
     count = 0;
+    disButton(true);
+    onStart.disabled = false;
+    document.querySelector('.score span').innerHTML = "0";
+    document.getElementById("time").innerHTML = "10";
     createQuestionElement(count);
-    clearInterval(countdown);
 }
 // Show Correct or Incorrect// check true false
 var showMessage = document.querySelector('.showMessage');
@@ -53,6 +50,7 @@ var count = 0;
 var score = 0;
 function reply_click(clicked_id) {
     if (checkEnd(count)) {
+        clearInterval(countdown);
         notification.style.display = "inline";
         restart()
     } else {
@@ -62,7 +60,7 @@ function reply_click(clicked_id) {
             score++;
             countScore(score);
             showMessage.innerHTML = "<h3>Correct!!!</3>";
-            showMessage.style.backgroundColor = "#8bd3dd";
+            showMessage.style.backgroundColor = "#2cb67d";
             showMessage.style.display = "inline";
             setTimeout(function () {
                 showMessage.style.display = "none";
@@ -79,14 +77,15 @@ function reply_click(clicked_id) {
         createQuestionElement(count);
     }
 }
-// Created new question
+// Created new question - OK
 function createQuestionElement(index) {
     var question = quiz[index].question;
-    var choiccc = quiz[index].choices;
+    var choices = quiz[index].choices;
     quesDetail.innerHTML = question;
-    for (let i = 0; i < choiccc.length; i++) {
+    for (let i = 0; i < choices.length; i++) {
         var butt = document.getElementById('but' + i);
-        butt.innerHTML = choiccc[i];
+        console.log(butt);
+        butt.innerHTML = choices[i];
     }
 }
 // Display score
@@ -94,18 +93,15 @@ function countScore(score) {
     document.querySelector('.score span').innerHTML = score;
     document.getElementById('noti__score').innerHTML = score;
 }
-// Disabled button
+// Disabled button - OK
 function disButton(bool) {
     var elems = document.getElementsByClassName("answer__btn");
     for (var i = 0; i < elems.length; i++) {
         elems[i].disabled = bool;
     }
 }
-// Check WIn
+// Check WIn - OK
 function checkEnd(x) {
-    //get ra số câu hỏi
-    //so sánh số câu hỏi với count
-    // nếu count = length ques thì dừng
     var totalQuestion = quiz.length - 1;
     if (totalQuestion == x) {
         return true;
